536d076e864b0bbd8e27c07e931003f8fcb17819
Mike Szczys <szczys@hotmail.com>
2022-11-06 17:06:40 -0600

    Grains for bits 1 and 2

--- grains.asm
+++ grains.asm
@@ -49,14 +49,15 @@ GOTO frames
 frames:
   ; iterate from 14..0
   ; start from 15. it will dec before starting loop
-  MOV R2,0b1111
+  MOV R2,0b1110
   ; locate grain in row
   findgrain:
   ; Check column tracker
-  MOV R6,R0
-  CP R0,0
+  MOV R0,R6
+  CP R0,2 ; When this==max_columns, we're done scanning
   SKIP Z,2
   GOTO process_columns
+  MOV R6,0b1111
   ; check if R2 is zero
   MOV R0,R2
   CP R0,0
@@ -93,7 +94,6 @@ check_one:
   ; grain found
   INC R2
   MOV R0,[r1:r2]
-  MOV R7,R0 ; copy to r7 for subroutine access
   ; check below
   BIT R0,1
   SKIP NZ,2
@@ -135,7 +135,6 @@ check_two:
   ; grain found
   INC R2
   MOV R0,[r1:r2]
-  MOV R7,R0 ; copy to r7 for subroutine access
   ; check below
   BIT R0,2
   SKIP NZ,2
@@ -146,6 +145,7 @@ check_two:
   BIT R0,3
   SKIP NZ,2
   GOTO two_availthree
+
   DEC R2
   GOTO findgrain ; grain below, do nothing
       ; check below right
