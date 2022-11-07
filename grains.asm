5afb21fd65edbf8a06ee8202801183b9e672a5c9
Mike Szczys <szczys@hotmail.com>
2022-11-07 10:19:10 -0600

    Implement left to right page break

--- grains.asm
+++ grains.asm
@@ -1,3 +1,18 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;
+; Wasting Time
+; by Mike Szczys
+; MIT License 2022
+;
+; Cellular automata to simulate sand grains of an hourglass on the 8x16 LED
+; display of the 2022 Hackaday Superconference badge
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Register map:
+; R1 = upper page address
+; R2 = lower page address
+; R6 = column address [0..3]
+; R8 = loop counter (create bit every 3 loops)
+
 start:
 ; set slow clock speed
 MOV R0,0b0100
@@ -12,11 +27,12 @@ MOV R0,0b0000 ; zeros to copy to registers
 MOV R1,0b0010 ; upper nibble address
 MOV R2,0b1111 ; lower nibble address
 
-; display page
+; clear page 2
 MOV [R1:R2],R0
 DSZ R2
 JR [0b1111:0b1101]
 MOV [R1:R2],R0
+; clear page 3
 INC R1
 MOV [R1:R2],R0
 DSZ R2
@@ -38,21 +54,27 @@ INC R8
 MOV R0,R8
 ; check timer for creation frame
 CP R0,0b0011  ; Compare counter to 3
-SKIP Z,2 ; Skip next 3 lines if R0 < 3
+SKIP Z,2 ; Skip next 3 lines if R0 == 3
 GOTO frames
 
   ; check for full hourglass
+  ;   this will restart the app when the display is full, but only when the
+  ;   grain being created is on lower page, bit 3
+  ;   FIXME: make grain generation location and rate variable
+  ;          physics for this work, but the creation/restart logic doesn't
   MOV R0,[R1:R2]
   BIT R0,3
   SKIP Z,2
   GOTO start
 
   ; create grain
-  MOV R0,0b1000 ; Use for grain creation
+  MOV R0,0b1000
   MOV [R1:R2],R0
-  MOV R8,0b0000 ; Restart timer for next run
+  ; Restart timer for next run
+  MOV R8,0b0000
 
-  ; FIXME: create grain on second page
+; FIXME: testing to create grain on second page
+;        this won't be checked for a full display
 ;  INC R1
 ;  MOV R0,0b0100
 ;  MOV [R1:R2],R0
@@ -60,7 +82,6 @@ GOTO frames
 
 
 ; service movement frames
-
 frames:
   ; iterate from 14..0
   ; start from 15. it will dec before starting loop
@@ -69,19 +90,24 @@ frames:
   findgrain:
   ; Check column tracker
   MOV R0,R6
-  CP R0,3 ; When this==max_columns, we're done scanning
+  CP R0,3 ; When R6==bit3, we're done scanning
   SKIP Z,2
-  GOTO process_columns
-  MOV R6,0b1111
+  GOTO process_columns ; Keep iterating columns
+  MOV R6,0b1111 ; Restart column tracker
+
   ; check if R2 is zero
   MOV R0,R2
   CP R0,0
   SKIP Z,2
-  GOTO continueframes
+  GOTO continueframes ; R2 is not zero, keep checking rows
+
+  ; R2 is zero. Check if we need to increment pages
   MOV R0,R1
   CP R0,2
   SKIP Z,2
-  GOTO loop
+  GOTO loop ; Both pages are done, start the loop anew
+
+  ; Lower page is finished so run again for upper page
   INC R1
   MOV R2,0b1111
   ; end zero check
@@ -126,11 +152,28 @@ check_zero:
   BIT R0,1
   SKIP NZ,2
   GOTO zero_availone
+
+  ;Check if left page
+  MOV R0,R1
+  CP R0,3
+  SKIP Z,3
   DEC R2
-  GOTO findgrain ; grain below, do nothing
-      ; check below right
-      ; check below left
-      ; move if space
+  GOTO findgrain ; This is not an edge-case; move along
+
+  ; This the right edge of the left page, check across page break
+  DEC R1
+  MOV R0,[R1:R2]
+  BIT R0,3
+  SKIP Z,0
+  INC R1
+  DEC R2
+  GOTO findgrain ; there's no room, reset registers and move along
+
+  ; We found room, set new grain and erase old
+  GOSUB setthree
+  INC R1 ; Reset upper page address (done with page break operations)
+  GOTO erasezero ; found space
+
   ; draw new grain
   zero_availzero:
   GOSUB setzero
@@ -163,10 +206,8 @@ check_one:
   GOTO one_availtwo
   DEC R2
   GOTO findgrain ; grain below, do nothing
-      ; check below right
-      ; check below left
-      ; move if space
-  ; draw new grain
+
+  ; draw new grain, erase old
   one_availzero:
   GOSUB setzero
   GOTO eraseone
@@ -205,10 +246,8 @@ check_two:
 
   DEC R2
   GOTO findgrain ; grain below, do nothing
-      ; check below right
-      ; check below left
-      ; move if space
-  ; draw new grain
+
+  ; draw new grain, erase old
   two_availone:
   GOSUB setone
   GOTO erasetwo
@@ -234,33 +273,34 @@ check_three:
   ; check below
   BIT R0,3
   SKIP NZ,2
-  GOTO three_availthree
-;  BIT R0,0
-;  SKIP NZ,2
-;  GOTO one_availzero
+  GOTO three_availthree ; found space
+
   BIT R0,2
   SKIP NZ,2
-  GOTO three_availtwo
-  DEC R2
+  GOTO three_availtwo ; found space
+
   ;Check if right page
   MOV R0,R1
   CP R0,2
-  SKIP Z,2
-  GOTO findgrain ; grain below, do nothing
+  SKIP Z,3
+  DEC R2
+  GOTO findgrain ; This is not an edge-case; move along
 
+  ; This the left edge of the right page, check across page break
   INC R1
   MOV R0,[R1:R2]
   BIT R0,0
-  SKIP Z,3
+  SKIP Z,0
   DEC R1
-  GOTO findgrain
+  DEC R2
+  GOTO findgrain ; there's no room, reset registers and move along
+
+  ; We found room, set new grain and erase old
   GOSUB setzero
-  DEC R1
-  GOTO erasethree
-      ; check below right
-      ; check below left
-      ; move if space
-  ; draw new grain
+  DEC R1 ; Reset upper page address (done with page break operations)
+  GOTO erasethree ; found space
+
+  ; draw new grain, erase old
   three_availtwo:
   GOSUB settwo
   GOTO erasethree
